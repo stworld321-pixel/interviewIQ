138c0382f4c59d4e2ff62b419f37d108ed047c04
stworld321-pixel <stworld321@gmail.com>
2026-02-26 14:17:59 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,7 +13,7 @@ import InterviewReport from './pages/InterviewReport'
 import AboutUs from './pages/AboutUs'
 import ContactUs from './pages/ContactUs'
 
-export const ServerUrl  = import.meta.env.VITE_API_URL || "https://interviewiq-fc93.onrender.com"
+export const ServerUrl  = import.meta.env.VITE_API_URL || "https://interviewiq-1-server.onrender.com"
 
 function ProtectedRoute({ children }) {
   const { userData, authChecked } = useSelector((state) => state.user)
