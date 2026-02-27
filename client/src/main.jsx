559362e989bbe2f81151f685bea842c31ebff58d
stworld321-pixel <stworld321@gmail.com>
2026-02-27 15:55:30 +0530

    white issues

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,6 +6,14 @@ import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './redux/store.js'
 import AppErrorBoundary from './components/AppErrorBoundary.jsx'
+
+const params = new URLSearchParams(window.location.search)
+const redirectedPath = params.get('p')
+if (redirectedPath) {
+  const cleanedPath = decodeURIComponent(redirectedPath)
+  window.history.replaceState(null, '', cleanedPath)
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AppErrorBoundary>
