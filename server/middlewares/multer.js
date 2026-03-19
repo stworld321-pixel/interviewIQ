ef987be9ad5f6f7f474cdf9df8bd3283b9fa7116
stworld321-pixel <stworld321@gmail.com>
2026-03-19 10:07:48 +0530

    image update

--- server/middlewares/multer.js
+++ server/middlewares/multer.js
@@ -1,11 +1,19 @@
 import multer from "multer";
 
+const toSafeFileName = (name = "") =>
+  name
+    .normalize("NFKD")
+    .replace(/[^\w.\-]/g, "_")
+    .replace(/_+/g, "_")
+    .replace(/^_+|_+$/g, "");
+
 const storage = multer.diskStorage({
     destination: function(req, file , cb){
         cb(null , "public")
     },
     filename: function(req , file , cb){
-        const filename = Date.now() + "-" + file.originalname;
+        const safeOriginal = toSafeFileName(file.originalname || "file");
+        const filename = `${Date.now()}-${safeOriginal}`;
         cb(null , filename)
     }
 })
